0d8a33229ef3f2f6812b1c9cd4e236ec7253c204
Amir Reza Ranjbar <amirsparrowranjbar@gmail.com>
2022-07-28 22:51:45 +0430

    Initialize

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Whats your weight?");
+int weight = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Whats your height(meter)?");
+float height = float.Parse(Console.ReadLine().ToString());
+
+float bodyMassIndex = weight / height * height;
+Console.WriteLine($"Weight index: {bodyMassIndex}");
+if (bodyMassIndex > 18 && bodyMassIndex < 23)
+{
+    Console.WriteLine("Your body massindex control");
+}
+Console.ReadKey();
